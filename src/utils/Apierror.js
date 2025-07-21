07284206b95b047f6480cbc61e850deeb0c5a297
octaveweb <karanswarnakar955@gmail.com>
2025-07-21 18:56:12 +0530

    error

--- src/utils/Apierror.js
+++ src/utils/Apierror.js
@@ -12,7 +12,7 @@ class ApiError extends Error{
          this.success = false
          this.errors = errors
 
-         if (statck) {
+         if (this.stack) {
             this.stack = statck
          }else{
             Error.captureStackTrace(this,this.constructor)
